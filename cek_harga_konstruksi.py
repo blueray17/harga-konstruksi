c6b7594cb506b008c3eaa673fc9a717851e1ed1d
ipin-jp <58415521+blueray17@users.noreply.github.com>
2026-07-30 10:25:27 +0700

    Update Google Sheet ID in cek_harga_konstruksi.py

--- cek_harga_konstruksi.py
+++ cek_harga_konstruksi.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Check Harga Konstruksi", layout="centered", page_
 # ============================================================
 # KONFIGURASI GOOGLE SHEET
 # ============================================================
-SHEET_ID = "1uuuZhUlzolwlxBx5lothgtBeEr4t5SuK"
+SHEET_ID = "1W48WatlQA_p8UQ4vQKHcO1HUBOXqZFvTqeybQk_jYrI"
 GID = "0"
 SHEET_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv&gid={GID}"
 
